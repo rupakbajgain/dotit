90d7be198b540ce761d4d2fb9da2ee8102a97e46
Rupak Bajgain <nnew234567@gmail.com>
2024-11-27 15:01:14 +0545

    Run main.lua

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <sol/sol.hpp>
 
 int main(int argc, char* argv[]) {
-
   sol::state lua;
+
   lua.open_libraries(sol::lib::base);
 
-  lua.script("print('bark bark bark!')");
+  lua.script_file("main.lua");
 
   return 0;
 }
